79f76a6d5a9b1cba228f866758133bb7ed1ab409
dwesty17 <j.dylan.westerhold@gmail.com>
2019-12-01 16:01:02 -0800

    Add redirect and checkLoggedIn helpers

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
-import React from "react"
+import React from "react";
+import { useApolloClient } from "@apollo/react-hooks";
 
-import "../styles.scss"
+import "../styles.scss";
+import redirect from "../lib/redirect";
+import checkLoggedIn from "../lib/checkLoggedIn";
 
-const Dashboard = () => (
-    <div>
-        TODO Dashboard
-    </div>
-);
+const Dashboard = ({ loggedInUser }) => {
+    const apolloClient = useApolloClient();
+
+    return (
+        <div>
+            TODO Dashboard
+        </div>
+    );
+};
+
+Dashboard.getInitialProps = async (context) => {
+    const { loggedInUser } = await checkLoggedIn(context.apolloClient);
+
+    if (!loggedInUser.user) {
+        redirect(context, "/login");
+    }
+
+    return { loggedInUser }
+};
 
 export default Dashboard
