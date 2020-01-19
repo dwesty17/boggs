46205b486fb7e5d6eb21fac1f499f68c177418eb
dwesty17 <j.dylan.westerhold@gmail.com>
2020-01-19 13:18:42 -0800

    Update variable names to reflect new periods

--- components/SpendingPerDay/SpendingPerDay.js
+++ components/SpendingPerDay/SpendingPerDay.js
@@ -9,26 +9,26 @@ import PercentageBar from "../PercentageBar";
 
 const SPENDING_QUERIES = gql`
     query GetAmountsSpent(
-        $from5: String!,
-        $from10: String!,
-        $from20: String!,
+        $from7: String!,
+        $from14: String!,
+        $from30: String!,
     ) {
-        spendingIn5: getAmountSpent(from: $from5)
-        spendingIn10: getAmountSpent(from: $from10)
-        spendingIn20: getAmountSpent(from: $from20)
+        spendingIn7: getAmountSpent(from: $from7)
+        spendingIn14: getAmountSpent(from: $from14)
+        spendingIn30: getAmountSpent(from: $from30)
     }
 `;
 
 const SpendingPerDay = ({monthlySpendingGoal}) => {
-    const fiveDaysAgo = moment().subtract(4, "days").startOf("day").valueOf().toString();
-    const tenDaysAgo = moment().subtract(9, "days").startOf("day").valueOf().toString();
-    const twentyDaysAgo = moment().subtract(19, "days").startOf("day").valueOf().toString();
+    const sevenDaysAgo = moment().subtract(6, "days").startOf("day").valueOf().toString();
+    const tenDaysAgo = moment().subtract(13, "days").startOf("day").valueOf().toString();
+    const twentyDaysAgo = moment().subtract(29, "days").startOf("day").valueOf().toString();
 
     const {loading, error, data} = useQuery(SPENDING_QUERIES, {
         variables: {
-            from5: fiveDaysAgo,
-            from10: tenDaysAgo,
-            from20: twentyDaysAgo,
+            from7: sevenDaysAgo,
+            from14: tenDaysAgo,
+            from30: twentyDaysAgo,
         },
     });
 
@@ -42,38 +42,38 @@ const SpendingPerDay = ({monthlySpendingGoal}) => {
     }
 
     const {
-        spendingIn5,
-        spendingIn10,
-        spendingIn20,
+        spendingIn7,
+        spendingIn14,
+        spendingIn30,
     } = data;
 
     const dailyGoal = getDailyGoal(monthlySpendingGoal);
 
-    const goalFor5 = getGoalForPeriod(dailyGoal, 5);
-    const goalFor10 = getGoalForPeriod(dailyGoal, 10);
-    const goalFor20 = getGoalForPeriod(dailyGoal, 20);
+    const goalFor7 = getGoalForPeriod(dailyGoal, 7);
+    const goalFor14 = getGoalForPeriod(dailyGoal, 14);
+    const goalFor30 = getGoalForPeriod(dailyGoal, 30);
 
-    const percentageFor5 = percentageOfGoal(spendingIn5, goalFor5);
-    const percentageFor10 = percentageOfGoal(spendingIn10, goalFor10);
-    const percentageFor20 = percentageOfGoal(spendingIn20, goalFor20);
+    const percentageFor7 = percentageOfGoal(spendingIn7, goalFor7);
+    const percentageFor14 = percentageOfGoal(spendingIn14, goalFor14);
+    const percentageFor30 = percentageOfGoal(spendingIn30, goalFor30);
 
     return (
         <div className="spending-per-day-container">
             {monthlySpendingGoal && <h2>Spending</h2>}
 
             <div className="spending-per-day-progress-row">
-                <p>Past 5 days: ${spendingIn5.toFixed(2)} / ${goalFor5}</p>
-                <PercentageBar percentage={percentageFor5} />
+                <p>Past 7 days: ${spendingIn7.toFixed(2)} / ${goalFor7}</p>
+                <PercentageBar percentage={percentageFor7} />
             </div>
 
             <div className="spending-per-day-progress-row">
-                <p>Past 10 days: ${spendingIn10.toFixed(2)} / ${goalFor10}</p>
-                <PercentageBar percentage={percentageFor10} />
+                <p>Past 14 days: ${spendingIn14.toFixed(2)} / ${goalFor14}</p>
+                <PercentageBar percentage={percentageFor14} />
             </div>
 
             <div className="spending-per-day-progress-row">
-                <p>Past 20 days: ${spendingIn20.toFixed(2)} / ${goalFor20}</p>
-                <PercentageBar percentage={percentageFor20} />
+                <p>Past 30 days: ${spendingIn30.toFixed(2)} / ${goalFor30}</p>
+                <PercentageBar percentage={percentageFor30} />
             </div>
         </div>
     );
