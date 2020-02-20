ed9b83970c4d1baa98fa5cdb699322a903b8aff3
dwesty17 <j.dylan.westerhold@gmail.com>
2020-02-19 22:27:38 -0800

    reformatting

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 const withSass = require("@zeit/next-sass");
 
 const config = {
-    env: {
-        SERVER_URL: process.env.SERVER_URL,
-    },
+  env: {
+    SERVER_URL: process.env.SERVER_URL,
+  },
 };
 
 module.exports = withSass(config);
