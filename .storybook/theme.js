07bf56ed96f328fce223904c82389e665fda83c1
dwesty17 <j.dylan.westerhold@gmail.com>
2020-05-01 19:10:04 -0700

    Delete unused code in storybook theme

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -9,31 +9,9 @@ export default create({
   brandUrl: "https://github.com/dwesty17/pennyworth",
   brandImage: "logos/text-logo-ship-gray.png",
 
-  // colorPrimary: Color.HippieBlue,
-  // colorSecondary: Color.IceCold,
-  //
-  // // Typography
-  // fontBase: "Open Sans, sans-serif",
-  // fontCode: "monospace",
-  //
-  // UI
   appBg: Color.White,
   appContentBg: Color.White,
   appBorderColor: Color.White,
   appBorderRadius: 4,
-  //
-  // // Text colors
-  // textColor: Color.Black,
-  // textInverseColor: Color.ShipGrey,
-  //
-  // // Toolbar default and active colors
-  // barTextColor: Color.White,
-  // barSelectedColor: Color.ShipGrey,
   barBg: Color.GovernorsBay,
-  //
-  // // Form colors
-  // inputBg: Color.White,
-  // inputBorder: Color.ShipGrey,
-  // inputTextColor: Color.ShipGrey,
-  // inputBorderRadius: 4,
 });
