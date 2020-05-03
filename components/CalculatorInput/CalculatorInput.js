b84af9cc94d83ebd66b98158834687979129a517
dwesty17 <j.dylan.westerhold@gmail.com>
2020-05-02 21:47:26 -0700

    Move CalculatorInput into it's own directory

--- components/CalculatorInput/CalculatorInput.js
+++ components/CalculatorInput/CalculatorInput.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-import Input from "./Input";
+import Input from "../Input";
 
 const CalculatorInput = (props) => {
     const [previousValue, setPreviousValue] = useState("");
