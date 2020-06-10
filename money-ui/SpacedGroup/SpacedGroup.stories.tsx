5f64150522b7fc593eae9398e24639d543030981
twjwesterhold <thomas.westerhold@vanderbilt.edu>
2020-06-10 12:33:30 -0400

    Converted SpacedGroup to TypeScript

--- money-ui/SpacedGroup/SpacedGroup.stories.tsx
+++ money-ui/SpacedGroup/SpacedGroup.stories.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {action} from "@storybook/addon-actions";
 
 import { TextInput } from "../TextInput";
 
@@ -10,24 +11,51 @@ export default {
 
 export const Default = () => (
     <SpacedGroup>
-        <TextInput placeholder="input 1"/>
-        <TextInput placeholder="input 2"/>
-        <TextInput placeholder="input 3"/>
+        <TextInput
+            placeholder="input 1"
+            onChange={action("TextInput1#onChange")}
+        />
+        <TextInput 
+            placeholder="input 2"
+            onChange={action("TextInput2#onChange")}
+        />
+        <TextInput
+            placeholder="input 3"
+            onChange={action("TextInput3#onChange")}
+        />
     </SpacedGroup>
 );
 
 export const LessSpace = () => (
     <SpacedGroup space={5}>
-        <TextInput placeholder="input 1"/>
-        <TextInput placeholder="input 2"/>
-        <TextInput placeholder="input 3"/>
+        <TextInput
+            placeholder="input 1"
+            onChange={action("TextInput1#onChange")}
+        />
+        <TextInput 
+            placeholder="input 2"
+            onChange={action("TextInput2#onChange")}
+        />
+        <TextInput
+            placeholder="input 3"
+            onChange={action("TextInput3#onChange")}
+        />
     </SpacedGroup>
 );
 
 export const MoreSpace = () => (
     <SpacedGroup space={30}>
-        <TextInput placeholder="input 1"/>
-        <TextInput placeholder="input 2"/>
-        <TextInput placeholder="input 3"/>
+        <TextInput
+            placeholder="input 1"
+            onChange={action("TextInput1#onChange")}
+        />
+        <TextInput 
+            placeholder="input 2"
+            onChange={action("TextInput2#onChange")}
+        />
+        <TextInput
+            placeholder="input 3"
+            onChange={action("TextInput3#onChange")}
+        />
     </SpacedGroup>
 );
